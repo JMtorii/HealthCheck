02887988d1373ffae6321f51cdd61bcf5d72415d
Jun Mark Torii <j.torii4@gmail.com>
2017-05-24 21:53:27 -0400

    First commit!

---
+++ Pods/Target Support Files/Pods-HealthCheckTests/Pods-HealthCheckTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HealthCheckTests : NSObject
+@end
+@implementation PodsDummy_Pods_HealthCheckTests
+@end
---
+++ Pods/Target Support Files/Pods-HealthCheckUITests/Pods-HealthCheckUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HealthCheckUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HealthCheckUITests
+@end
